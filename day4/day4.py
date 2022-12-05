3c217672d388e5a0934d7267eff4e337cd9049d6
John Mantios <gmantios@gmail.com>
2022-12-06 00:01:20 +0200

    Day 4, python

---
+++ day4/day4.py
@@ -0,0 +1,57 @@
+def solution_first_part(path: str) -> None:
+    score = 0
+    with open(file=path, newline="\n") as input_data:
+        for row in input_data:
+            row = row.rstrip("\n")
+            groups = row.split(",")
+            groups = [x.split("-") for x in groups]
+            if (
+                int(groups[0][0]) >= int(groups[1][0])
+                and int(groups[0][1]) <= int(groups[1][1])
+            ) or (
+                int(groups[0][0]) <= int(groups[1][0])
+                and int(groups[0][1]) >= int(groups[1][1])
+            ):
+                score += 1
+            else:
+                continue
+
+    print(score)
+
+
+def solution_second_part(path: str) -> None:
+    score = 0
+    with open(file=path, newline="\n") as input_data:
+        for row in input_data:
+            row = row.rstrip("\n")
+            groups = row.split(",")
+            groups = [x.split("-") for x in groups]
+            if (
+                (
+                    int(groups[0][0]) >= int(groups[1][0])
+                    and int(groups[0][1]) <= int(groups[1][1])
+                )
+                or (
+                    int(groups[0][0]) <= int(groups[1][0])
+                    and int(groups[0][1]) >= int(groups[1][1])
+                )
+                or int(groups[0][1]) == int(groups[1][0])
+                or (
+                    int(groups[0][1]) > int(groups[1][0])
+                    and int(groups[0][1]) < int(groups[1][1])
+                )
+                or (
+                    int(groups[0][0]) < int(groups[1][1])
+                    and int(groups[0][1]) > int(groups[1][1])
+                )
+                or int(groups[0][0]) == int(groups[1][1])
+            ):
+                score += 1
+            else:
+                continue
+
+    print(score)
+
+
+solution_first_part("/Users/johnmantios1/Desktop/advent_of_code_2022/day4/input4.txt")
+solution_second_part("/Users/johnmantios1/Desktop/advent_of_code_2022/day4/input4.txt")
